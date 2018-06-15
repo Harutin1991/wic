61c2082519282f9429e488baff01801b0c6aca14
Harut <harut.soghomonyan@gmail.com>
2018-06-15 17:06:35 +0400

    Database

---
+++ database/wic_db.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2018 at 03:00 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wic_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `abbreviation` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`, `abbreviation`) VALUES
+(1, 'Afghanistan', 'AF'),
+(2, 'Albania', 'AL'),
+(3, 'Algeria', 'DZ'),
+(4, 'American Samoa', 'AS'),
+(5, 'Andorra', 'AD'),
+(6, 'Angola', 'AO'),
+(7, 'Anguilla', 'AI'),
+(8, 'Antarctica', 'AQ'),
+(9, 'Antigua And Barbuda', 'AG'),
+(10, 'Argentina', 'AR'),
+(11, 'Armenia', 'AM'),
+(12, 'Aruba', 'AW'),
+(13, 'Australia', 'AU'),
+(14, 'Austria', 'AT'),
+(15, 'Azerbaijan', 'AZ'),
+(16, 'Bahamas The', 'BS'),
+(17, 'Bahrain', 'BH'),
+(18, 'Bangladesh', 'BD'),
+(19, 'Barbados', 'BB'),
+(20, 'Belarus', 'BY'),
+(21, 'Belgium', 'BE'),
+(22, 'Belize', 'BZ'),
+(23, 'Benin', 'BJ'),
+(24, 'Bermuda', 'BM'),
+(25, 'Bhutan', 'BT'),
+(26, 'Bolivia', 'BO'),
+(27, 'Bosnia and Herzegovina', 'BA'),
+(28, 'Botswana', 'BW'),
+(29, 'Bouvet Island', 'BV'),
+(30, 'Brazil', 'BR'),
+(31, 'British Indian Ocean Territory', 'IO'),
+(32, 'Brunei', 'BN'),
+(33, 'Bulgaria', 'BG'),
+(34, 'Burkina Faso', 'BF'),
+(35, 'Burundi', 'BI'),
+(36, 'Cambodia', 'KH'),
+(37, 'Cameroon', 'CM'),
+(38, 'Canada', 'CA'),
+(39, 'Cape Verde', 'CV'),
+(40, 'Cayman Islands', 'KY'),
+(41, 'Central African Republic', 'CF'),
+(42, 'Chad', 'TD'),
+(43, 'Chile', 'CL'),
+(44, 'China', 'CN'),
+(45, 'Christmas Island', 'CX'),
+(46, 'Cocos (Keeling) Islands', 'CC'),
+(47, 'Colombia', 'CO'),
+(48, 'Comoros', 'KM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `places`
+--
+
+CREATE TABLE `places` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `zip_id` int(11) NOT NULL,
+  `country_id` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zip_codes`
+--
+
+CREATE TABLE `zip_codes` (
+  `id` int(11) NOT NULL,
+  `zip_code` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `places`
+--
+ALTER TABLE `places`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `country_id` (`country_id`);
+
+--
+-- Indexes for table `zip_codes`
+--
+ALTER TABLE `zip_codes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
+-- AUTO_INCREMENT for table `places`
+--
+ALTER TABLE `places`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `zip_codes`
+--
+ALTER TABLE `zip_codes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
